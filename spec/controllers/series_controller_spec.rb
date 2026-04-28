1172cd640b6299def55b047b46fdb22aa57d552c
Céline BERTAUD <celine.bertaud@opisto.com>
2026-04-28 20:34:26 +0200

    AO3-7410 Fix the inconsistency in the site name on the page title for works and series

--- spec/controllers/series_controller_spec.rb
+++ spec/controllers/series_controller_spec.rb
@@ -257,7 +257,7 @@ describe SeriesController do
       work = create(:work, fandom_string: "Fandom", authors: [user.default_pseud])
       series_with_work = create(:series, works: [work], authors: [user.default_pseud])
       get :show, params: { id: series_with_work }
-      expect(assigns[:page_title]).to eq("#{series_with_work.title} - #{user.default_pseud.name} - Fandom [#{ArchiveConfig.APP_NAME}]")
+      expect(assigns[:page_title]).to eq("#{series_with_work.title} - #{user.default_pseud.name} - Fandom | #{ArchiveConfig.APP_NAME}")
     end
 
     it "assigns page title for anonymous series" do
@@ -265,7 +265,7 @@ describe SeriesController do
       anonymous_work = create(:work, fandom_string: "Fandom", collections: [anonymous_collection])
       series_with_work = create(:series, works: [anonymous_work])
       get :show, params: { id: series_with_work }
-      expect(assigns[:page_title]).to eq("#{series_with_work.title} - Anonymous - Fandom [#{ArchiveConfig.APP_NAME}]")
+      expect(assigns[:page_title]).to eq("#{series_with_work.title} - Anonymous - Fandom | #{ArchiveConfig.APP_NAME}")
     end
 
     it "assigns page subtitle for unrevealed series" do
